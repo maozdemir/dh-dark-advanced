7365e0ff1d2c5ab100131adeea12bedc77a2d859
impulsivus <alperozdemir@mail.com>
2021-08-20 06:19:50 +0300

    Rewrite userscript

--- dhdarkadvanced.user.js
+++ dhdarkadvanced.user.js
@@ -10,117 +10,207 @@
 // @grant        GM.setValue
 // @grant        GM.xmlHttpRequest
 // @resource     DH_DARK https://github.com/impulsiva/dh-dark-advanced/raw/beta/internal/forum.style.css
+// @resource     DH_LIGHT https://github.com/impulsiva/dh-dark-advanced/raw/beta/internal/forum.style.light.css?ver=6
 // @grant           GM_addStyle
 // @grant           GM_getResourceText
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
     var cssTxt = GM_getResourceText ("DH_DARK");
-    GM_addStyle (cssTxt);
+    var cssLightTxt = GM_getResourceText ("DH_LIGHT");
+        var ValDark = await GM.getValue("TTL_Dark", false);
+    if(ValDark) GM_addStyle (cssTxt);
+    if(!ValDark) GM_addStyle (cssLightTxt);
 
-    window.onload = function() {
+        var ValCevap = await GM.getValue("TTL_Cevap", false);
+        var ValPM = await GM.getValue("TTL_PM", false);
+        var ValFav = await GM.getValue("TTL_Fav", false);
+        var ValGec = await GM.getValue("TTL_Gec", false);
+        var ValFrm = await GM.getValue("TTL_Frm", false);
+        var ValBO = await GM.getValue("TTL_BO", false);
+        var ValSB = await GM.getValue("TTL_SB", false);
+
+    window.onload = async function() {
+
+        if(ValCevap) {
+            $("#login_header > div > div.sag > div.ana-menu > a:nth-child(1)").attr("style", "display: none !important");
+        }
+        if(ValPM) {
+            $("#login_header > div > div.sag > div.ana-menu > a:nth-child(2)").attr("style", "display: none !important");
+        }
+        if(ValFav) {
+            $("#login_header > div > div.sag > div.ana-menu > a:nth-child(3)").attr("style", "display: none !important");
+        }
+        if(ValGec) {
+            $("#login_header > div > div.sag > div.ana-menu > a:nth-child(4)").attr("style", "display: none !important");
+        }
+        if(ValFrm) {
+            $("#login_header > div > div.sag > div.ana-menu > span:nth-child(5)").attr("style", "display: none !important");
+        }
+        if(ValBO) {
+            $("#login_header > div > div.sag > div.ana-menu > span:nth-child(6)").attr("style", "display: none !important");
+        }
+        if(ValSB) {
+            $("#login_header > div > div.foruma-git").attr("style", "display: none !important");
+        }
+
 
         $('body').append('<div class="TTL_CustomToolbar"></div>');
+        var myVersion = GM_info.script.version;
+        var settingsIcon = "https://raw.githubusercontent.com/impulsivus/DH-Gece-Modu/master/internals/baseline_settings_white_18dp.png";
+        var headBar = "<div class=\"TTL_ToolboxHead\">"+
+            "<div class=\"settings\">"+
+            //"<a href=\"#\" id=\"TTL_SettingsButton\">"+
+            "<input type=\"image\" src=\""+settingsIcon+"\" id=\"TTL_SettingsButton\">"+
+           // "</a>"+
+            "</div>"+
+            "<div class=\"text\">"+
+            "DH Dark Advanced <span>"+myVersion+"</span>"+
+            "</div>"+
+            "</div>";
+        $('.TTL_CustomToolbar').append(headBar);
+
 
         var currentPage = $($('body')).html();
-        console.log(currentPage);
+        //var forumIDregex = new RegExp(/\/api2\/GlobalApi\/GetReplyForm\?forumID\=([a-z])\w+\&amp\;/g);
         var forumIDregex = new RegExp(/GetReplyForm\?forumID\=(.*?)\&/g);
-        if(forumIDregex == null) forumIDregex = new RegExp(/yeni-konu-(.*?)\" class\=\"kl-btn/g);
-
-        var forumID = forumIDregex.exec(currentPage)[1];
-        console.log("REGEX RESULT: "+forumIDregex.exec(currentPage)[1]);
-        $.get("https://forum.donanimhaber.com/api2/GlobalApi/gettopics?forumId="+forumID+"&page=1&filter=", function( data ) {
-            var userData = data["Data"]["Html"];
-            var elements = $($.parseHTML(userData)).find("h3");
-            var elementsWithAds = $($.parseHTML(userData)).find(".kl-konu");
-            var adsCount = (elementsWithAds.length - elements.length);
-            console.log("Ads count:"+adsCount);
-            var elements_replies = $($.parseHTML(userData)).find(".kl-cevap span");
-            var injectContent ="";
-            $.each(elements, function(i,v){
-                if(i != 0) {
-                    var iwa = i+adsCount-1;
-                    var content = "<div class=\"TTL_ToolboxItem\">" + v.parentElement.outerHTML + " <span>(" + elements_replies[iwa].innerHTML + ")</span></div>";
-                    injectContent += content;
-                    console.log(content);
-                    console.log("i"+i+"iwa"+iwa);
-                }
-            });
-            $('.TTL_CustomToolbar').html(injectContent);
-            console.log( "Load was performed for Page1.");
-        });
-        $.get("https://forum.donanimhaber.com/api2/GlobalApi/gettopics?forumId="+forumID+"&page=2&filter=", function( data ) {
-            var userData = data["Data"]["Html"];
-            var elements = $($.parseHTML(userData)).find("h3");
-            var elementsWithAds = $($.parseHTML(userData)).find(".kl-konu");
-            var adsCount = (elementsWithAds.length - elements.length);
-            console.log("Ads count:"+adsCount);
-            var elements_replies = $($.parseHTML(userData)).find(".kl-cevap span");
-            var injectContent ="";
-            $.each(elements, function(i,v){
-                if(i != 0) {
-                    var iwa = i+adsCount-1;
-                    var content = "<div class=\"TTL_ToolboxItem\">" + v.parentElement.outerHTML + " <span>(" + elements_replies[iwa].innerHTML + ")</span></div>";
-                    injectContent += content;
-                    console.log(content);
-                    console.log("i"+i+"iwa"+iwa);
-                }
-            });
-            $('.TTL_CustomToolbar').append(injectContent);
-            console.log( "Load was performed for Page1.");
-        });
-        const queryString = window.location.search;
-        console.log(queryString);
-
-
-
-    if(getUrlParameter(window.location.href)["configPage"] === "true") {
-        console.log("INIT xhr");
-        GM.xmlHttpRequest({
-            method: "GET",
-            url: "https://github.com/impulsiva/DH-Gece-Modu/raw/master/internals/config.html",
-            onload: function(response) {
-                var responseXML = null;
-
-                $(".dhorta").html(response.response);
-                console.log(response.response);
-                if (!response.responseXML) {
-                    responseXML = new DOMParser()
-                        .parseFromString(response.responseText, "text/xml");
-                }
-
-                console.log([
-                    response.status,
-                    response.statusText,
-                    response.readyState,
-                    response.responseHeaders,
-                    response.responseText,
-                    response.finalUrl,
-                    responseXML
-                ].join("\n"));
+        if(forumIDregex == "") {
+            forumIDregex = new RegExp(/yeni-konu-(.*?)\" class\=\"kl-btn/g);
+        }
+        var forumID;
+        if (forumIDregex.exec(currentPage) == null) {
+            forumID = 0;
+        } else {
+            forumID = forumIDregex.exec(currentPage)[1];
+        }
+        console.log("REGEX RESULT: "+forumID);
+        //var forumID = 2108;
+        if(forumID != 0) {
+            var kin = 1;
+            var maxPageOnLeftBar = 2;
+            var injectContent = "";
+            while(kin <= maxPageOnLeftBar) {
+                $.get("https://forum.donanimhaber.com/api2/GlobalApi/gettopics?forumId="+forumID+"&page="+kin+"&filter=", function( data ) {
+                    var userData = data["Data"]["Html"];
+                    var elements = $($.parseHTML(userData)).find("h3");
+                    var elementsWithAds = $($.parseHTML(userData)).find(".kl-konu");
+                    var adsCount = (elementsWithAds.length - elements.length);
+                    var elements_replies = $($.parseHTML(userData)).find(".kl-cevap span");
+                    $.each(elements, function(i,v){
+                        if(i != 0) {
+                            var iwa = i+adsCount-1;
+                            var content = "<div class=\"TTL_ToolboxItem\">" + v.parentElement.outerHTML + " <span>(" + elements_replies[iwa].innerHTML + ")</span></div>";
+                            $('.TTL_CustomToolbar').append(content);
+                            console.log(content);
+                        }
+                    });
+                });
+                kin++;
             }
+            console.log(injectContent);
+        }
+
+
+
+
+        $("#TTL_SettingsButton").click(function(){
+            var HTML_Settings = '<div class="TTL_Settings">'+
+                '<input type="checkbox" name="TTL_Dark" id="TTL_Dark"/>'+
+                '<label for="TTL_Dark">Dark mod</label><br/>'+
+                '<input type="checkbox" name="TTL_Cevap" id="TTL_Cevap"/>'+
+                '<label for="TTL_Cevap">"Cevaplanan" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_PM" id="TTL_PM"/>'+
+                '<label for="TTL_PM">"Özel Mesaj" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_Fav" id="TTL_Fav"/>'+
+                '<label for="TTL_Fav">"Favoriler" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_Gec" id="TTL_Gec"/>'+
+                '<label for="TTL_Gec">"Geçmiş" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_Frm" id="TTL_Frm"/>'+
+                '<label for="TTL_Frm">"Forumlarım" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_BO" id="TTL_BO"/>'+
+                '<label for="TTL_BO">"Bana Özel" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_SB" id="TTL_SB"/>'+
+                '<label for="TTL_SB">Arama kutusunu gizle</label><br/>'+
+                '<input type="button" value="Kaydet" id="TTL_Save"/></div>';
+            $(".TTL_CustomToolbar").html(HTML_Settings);
+                if(ValDark == true) { $('#TTL_Dark').prop("checked", true);}
+                if(ValCevap == true) { $('#TTL_Cevap').prop("checked", true);}
+                if(ValPM == true)  { $('#TTL_PM').prop("checked", true);}
+                if(ValFav == true)  { $('#TTL_Fav').prop("checked", true);}
+                if(ValGec == true)  { $('#TTL_Gec').prop("checked", true);}
+                if(ValFrm == true)  { $('#TTL_Frm').prop("checked", true);}
+                if(ValBO == true)  { $('#TTL_BO').prop("checked", true);}
+                if(ValSB == true)  { $('#TTL_SB').prop("checked", true);}
+            $("#TTL_Save").click(function() {
+                var TTL_Cevap  = $('#TTL_Cevap').is(":checked");
+                var TTL_PM     = $('#TTL_PM').is(":checked");
+                var TTL_Fav    = $('#TTL_Fav').is(":checked");
+                var TTL_Gec    = $('#TTL_Gec').is(":checked");
+                var TTL_Frm    = $('#TTL_Frm').is(":checked");
+                var TTL_BO     = $('#TTL_BO').is(":checked");
+                var TTL_SB     = $('#TTL_SB').is(":checked");
+                var TTL_Dark     = $('#TTL_Dark').is(":checked");
+                GM.setValue("TTL_Cevap", TTL_Cevap);
+                GM.setValue("TTL_PM", TTL_PM);
+                GM.setValue("TTL_Fav", TTL_Fav);
+                GM.setValue("TTL_Gec", TTL_Gec);
+                GM.setValue("TTL_Frm", TTL_Frm);
+                GM.setValue("TTL_BO", TTL_BO);
+                GM.setValue("TTL_SB", TTL_SB);
+                GM.setValue("TTL_Dark", TTL_Dark);
+                alert("Kaydedildi!");
+            });
         });
-        var HTML_Settings = '<div class="TTL_Settings"><input type="checkbox" name="TTL_Cevap" id="TTL_Cevap"/><label for="TTL_Cevap">"Cevaplanan" düğmesini gizle</label><br/><input type="checkbox" name="TTL_PM" id="TTL_PM"/><label for="TTL_PM">"Özel Mesaj" düğmesini gizle</label><br/><input type="checkbox" name="TTL_Fav" id="TTL_Fav"/><label for="TTL_Fav">"Favoriler" düğmesini gizle</label><br/><input type="checkbox" name="TTL_Gec" id="TTL_Gec"/><label for="TTL_Gec">"Geçmiş" düğmesini gizle</label><br/><input type="checkbox" name="TTL_Frm" id="TTL_Frm"/><label for="TTL_Frm">"Forumlarım" düğmesini gizle</label><br/><input type="checkbox" name="TTL_BO" id="TTL_BO"/><label for="TTL_BO">"Bana Özel" düğmesini gizle</label><br/><input type="checkbox" name="TTL_SB" id="TTL_SB"/><label for="TTL_SB">Arama kutusunu gizle</label><br/><input type="button" value="Kaydet" id="TTL_Save"/></div>';
-        $(".dhorta").html(HTML_Settings);
-        $("#TTL_Save").click(function() {
-            var TTL_Cevap  = $('#TTL_Cevap').is(":checked");
-            var TTL_PM     = $('#TTL_PM').is(":checked");
-            var TTL_Fav    = $('#TTL_Fav').is(":checked");
-            var TTL_Gec    = $('#TTL_Gec').is(":checked");
-            var TTL_Frm    = $('#TTL_Frm').is(":checked");
-            var TTL_BO     = $('#TTL_BO').is(":checked");
-            var TTL_SB     = $('#TTL_SB').is(":checked");
-            GM.setValue("TTL_Cevap", TTL_Cevap);
-            GM.setValue("TTL_PM", TTL_PM);
-            GM.setValue("TTL_Fav", TTL_Fav);
-            GM.setValue("TTL_Gec", TTL_Gec);
-            GM.setValue("TTL_Frm", TTL_Frm);
-            GM.setValue("TTL_BO", TTL_BO);
-            GM.setValue("TTL_SB", TTL_SB);
-            alert("Kaydedildi!");
-        });
-    }
+
+        /*if(getUrlParameter(window.location.href)["configPage"] === "true") {
+            var HTML_Settings = '<div class="TTL_Settings">'+
+                '<input type="checkbox" name="TTL_Dark" id="TTL_Dark"/>'+
+                '<label for="TTL_Dark">Dark mod</label><br/>'+
+                '<input type="checkbox" name="TTL_Cevap" id="TTL_Cevap"/>'+
+                '<label for="TTL_Cevap">"Cevaplanan" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_PM" id="TTL_PM"/>'+
+                '<label for="TTL_PM">"Özel Mesaj" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_Fav" id="TTL_Fav"/>'+
+                '<label for="TTL_Fav">"Favoriler" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_Gec" id="TTL_Gec"/>'+
+                '<label for="TTL_Gec">"Geçmiş" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_Frm" id="TTL_Frm"/>'+
+                '<label for="TTL_Frm">"Forumlarım" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_BO" id="TTL_BO"/>'+
+                '<label for="TTL_BO">"Bana Özel" düğmesini gizle</label><br/>'+
+                '<input type="checkbox" name="TTL_SB" id="TTL_SB"/>'+
+                '<label for="TTL_SB">Arama kutusunu gizle</label><br/>'+
+                '<input type="button" value="Kaydet" id="TTL_Save"/></div>';
+            $(".dhorta").html(HTML_Settings);
+                if(ValCevap == true) { $('#TTL_Cevap').prop("checked", true);}
+                if(ValPM == true)  { $('#TTL_PM').prop("checked", true);}
+                if(ValFav == true)  { $('#TTL_Fav').prop("checked", true);}
+                if(ValGec == true)  { $('#TTL_Gec').prop("checked", true);}
+                if(ValFrm == true)  { $('#TTL_Frm').prop("checked", true);}
+                if(ValBO == true)  { $('#TTL_BO').prop("checked", true);}
+                if(ValSB == true)  { $('#TTL_SB').prop("checked", true);}
+            $("#TTL_Save").click(function() {
+                var TTL_Cevap  = $('#TTL_Cevap').is(":checked");
+                var TTL_PM     = $('#TTL_PM').is(":checked");
+                var TTL_Fav    = $('#TTL_Fav').is(":checked");
+                var TTL_Gec    = $('#TTL_Gec').is(":checked");
+                var TTL_Frm    = $('#TTL_Frm').is(":checked");
+                var TTL_BO     = $('#TTL_BO').is(":checked");
+                var TTL_SB     = $('#TTL_SB').is(":checked");
+                var TTL_Dark     = $('#TTL_Dark').is(":checked");
+                GM.setValue("TTL_Cevap", TTL_Cevap);
+                GM.setValue("TTL_PM", TTL_PM);
+                GM.setValue("TTL_Fav", TTL_Fav);
+                GM.setValue("TTL_Gec", TTL_Gec);
+                GM.setValue("TTL_Frm", TTL_Frm);
+                GM.setValue("TTL_BO", TTL_BO);
+                GM.setValue("TTL_SB", TTL_SB);
+                GM.setValue("TTL_Dark", TTL_Dark);
+                alert("Kaydedildi!");
+            });
+        }*/
+
 
     }
 
